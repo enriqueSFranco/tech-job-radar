6817e865b3e86fdee5503db9ee6805596a5e460e
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-21 17:46:01 -0600

    chore: se elimino el archivo ya que no era necesario por el momento

--- backend/src/controllers/ScraperController.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response} from "express"
-import {AppError} from "../utils/AppError"
-import { JobDTO } from "../dtos/job.dto"
-import { IScraperService } from "../services/interfaces/IScraperService"
-
-class ScraperController {
-  constructor(private readonly service: IScraperService) {}
-}
