a012b79fbc20534d1a6a1f8c6f11910707cbec3d
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-21 18:32:05 -0600

    feat(frontend): estructura del componente card job

--- frontend/src/ui/components/card/Card.tsx
+++ frontend/src/ui/components/card/Card.tsx
@@ -1,4 +1,5 @@
 import type { Job } from "@/domain/jobs/types";
+import styles from "./Card.module.css"
 
 interface Props {
   data: Job;
@@ -6,10 +7,47 @@ interface Props {
 }
 
 export function Card({ data, recommendedVacancy }: Props) {
-  const {title} = data
+  const {
+    title,
+    location,
+    type,
+    salary,
+    posted_date,
+    experience_level,
+    company,
+    remote,
+    educations,
+    technologies,
+    details,
+  } = data;
+
+  const cardClassName = recommendedVacancy ? styles.recommendedCard : styles.regularCard
+  const infoList = [
+    {icon: '', text: location},
+    {icon: '', text: type},
+    {icon: '', text: salary},
+    {icon: '', text: experience_level},
+    {icon: '', text: remote},
+  ]
   return (
-    <div>
-      <h2>{title}</h2>
-    </div>
-  )
+    <article className={cardClassName}>
+      <header className={styles.header}>
+        <div className={styles.titleBlock}>
+          <h2 className={styles.title}>{title}</h2>
+          <h3 className={styles.company}>{company}</h3>
+        </div>
+        <span className={styles.date}>{posted_date}</span>
+      </header>
+      <div className={styles.details}>
+        <p>{details ?? "No hay detalles para esta vacante"}</p>
+      </div>
+      <footer className={styles.footer}>
+        <ul className={styles.infoList}>
+          {infoList.map(item => (
+            <li key={`item-${item.text}`} className={styles.infoItem}><span>{item.text}</span></li>
+          ))}
+        </ul>
+      </footer>
+    </article>
+  );
 }
