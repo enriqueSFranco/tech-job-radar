f9e0b885064ef15c1290ba6133b4c68f661f55fd
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-23 10:25:02 -0600

    chore(frontend): icones de la app

---
+++ frontend/src/ui/components/icons/index.tsx
@@ -0,0 +1,11 @@
+export { IcRadar } from './system/IcRadar';
+export { IcOffice } from './places/IcOffice';
+export { IcHomeWork } from './places/IcHomeWork';
+export { IcBookMark } from './interface/IcBookMark';
+export { IcUser } from './interface/IcUser';
+export { IcFunnel } from './interface/IcFunnel';
+export { IcSparkles } from './interface/IcSparkles';
+export { IcBriefCase } from './business/IcBriefCase';
+export { IcDollar } from './business/IcDollar';
+export { IcLocation } from './business/IcLocation';
+
