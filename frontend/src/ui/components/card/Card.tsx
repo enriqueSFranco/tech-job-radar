a34f59244f7a8de1c5100accb87f8b65e5c220de
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-23 18:53:03 -0600

    feat: componente base para la card

--- frontend/src/ui/components/card/Card.tsx
+++ frontend/src/ui/components/card/Card.tsx
@@ -1,13 +1,13 @@
-import type { Job } from "@/domain/jobs/types";
+import type { Job } from "@/types";
 import styles from "./Card.module.css";
-import {
-  IcBriefCase,
-  IcDollar,
-  IcHomeWork,
-  IcLocation,
-  IcOffice,
-  IcUser,
-} from "../icons/Icon";
+// import {
+//   IcBriefCase,
+//   IcDollar,
+//   IcHomeWork,
+//   IcLocation,
+//   IcOffice,
+//   IcUser,
+// } from "../icons";
 
 interface Props {
   data: Job;
@@ -32,16 +32,7 @@ export function Card({ data, recommendedVacancy }: Props) {
   const cardClassName = recommendedVacancy
     ? styles.recommendedCard
     : "p-4 flex flex-col justify-bwetween gap-2 w-full h-[200px] bg-white/5";
-  const infoList = [
-    { icon: <IcBriefCase />, text: type },
-    { icon: <IcUser />, text: experience_level },
-    {
-      icon: false ? <IcHomeWork /> : <IcOffice />,
-      text: false ? "home" : "office",
-    },
-    { icon: <IcDollar />, text: salary },
-    { icon: <IcLocation />, text: location, colSpan: 2 },
-  ];
+
   return (
     <article className={`${cardClassName}`}>
       <header className="flex items-center justify-between text-sm">
@@ -52,7 +43,7 @@ export function Card({ data, recommendedVacancy }: Props) {
         <span className="font-light text-sm">{salary}</span>
       </div>
       {details && (
-        <div className={styles.details}>
+        <div className="">
           <p>{details}</p>
         </div>
       )}
