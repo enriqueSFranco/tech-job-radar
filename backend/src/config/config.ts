630beb2466bf24a564877bee7d147e46dafc454b
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-29 16:23:53 -0600

    chore(backend): delete files

--- backend/src/config/config.ts
+++
@@ -1,1 +0,0 @@
-export const { OCC_BASE_URL, GLASSDOOR_BASE_URL, PORT } = process.env;
--- backend/src/repositories/IScraperRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Job } from "../models/job.model";
-
-export interface IScraperRepository {
-  fetchVacancies(keyword: string, location: string): Promise<Job[]> | [];
-  findByFilters(): Promise<Job[]> | [];
-}
