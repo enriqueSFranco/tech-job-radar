2571ef21193ba5c17c8c336a7b83a2965b012bfa
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-05-14 19:06:06 -0600

    feat: menu switch para mostrar las vacanes recomendadas y todas las vacantes

---
+++ frontend/src/ui/VacancySwitcher.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router";
+
+type TabOptions = {
+  id: string,
+    label: string,
+    icon: React.ReactNode,
+    href: string,
+}
+
+type TabSwitcherProps = {
+  tabs: TabOptions[]
+  defaultActiveTabId?: string
+  onTabChange?: (id: string) => void;
+}
+
+export function VacancySwitcher({tabs, defaultActiveTabId, onTabChange}: TabSwitcherProps) {
+  const [activeTab, setActiveTab] = useState(defaultActiveTabId || tabs.length > 0 ? tabs[0].id : "");
+  const [indicatorStyles, setIndicatorStyle] = useState({ left: 0, width: 0 });
+  const tabRefs = useRef<Record<string, HTMLLIElement | null>>({});
+
+  useEffect(() => {
+    const activeElement = tabRefs.current[activeTab];
+    if (activeElement) {
+      const { offsetWidth, offsetLeft } = activeElement;
+      setIndicatorStyle({ left: offsetLeft, width: offsetWidth });
+    }
+  }, [activeTab]);
+
+  const handleTabClick = (id: string) => {
+    onTabChange?.(id);
+  };
+
+  return (
+    <div className="grid place-content-center w-full mt-3">
+      <nav className="w-full m-auto">
+        <ul className="flex items-center justify-between gap-4">
+          {tabs.map(({ id, label, icon, href }) => {
+            const isActive = activeTab === id;
+            const colorClass = isActive ? "text-white" : "text-gray-400";
+            return (
+              <li
+                key={`optionId-${id}`}
+                ref={(el) => (tabRefs.current[id] = el)}
+                className="inline-block relative px-4 py-2"
+              >
+                {href ? (
+                  <Link
+                    to={href}
+                    className={`text-base flex items-center justify-between gap-1 ${colorClass}`}
+                    onClick={() => setActiveTab(id)}
+                  >
+                    {icon && icon}
+                    {label}
+                  </Link>
+                ) : (
+                  <button
+                    className={`text-base flex items-center gap-1 ${colorClass}`}
+                    onClick={() => handleTabClick(id)}
+                  >
+                    {icon}
+                    {label}
+                  </button>
+                )}
+              </li>
+            );
+          })}
+          <span
+            className="absolute bottom-0 h-[2px] bg-white transition-all duration-300"
+            style={{ left: indicatorStyles.left, width: indicatorStyles.width }}
+          ></span>
+        </ul>
+      </nav>
+    </div>
+  );
+}
