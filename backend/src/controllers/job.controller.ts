8fdddbb16adcbf5c8606a8982b834d3b42351336
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-02 16:29:28 -0600

    reafactor: se movio la logica de paginacion a job.service y se usa el logger.adapter

--- backend/src/controllers/job.controller.ts
+++ backend/src/controllers/job.controller.ts
@@ -1,32 +1,32 @@
-import {Response, Request} from "express"
-import { Job } from "../models/job.model";
+import { Response, Request } from "express";
 import { JobService } from "../services/job.service";
+import { WinstonLoggerAdapter } from "../adapters/logger.adapter";
 
 export class JobController {
+  private logger = new WinstonLoggerAdapter("job.controller.ts");
+
   constructor(private readonly service: JobService) {}
 
   getJobs = async (req: Request, res: Response) => {
     try {
-      const {page = 1, perPage = 20} = req.query
-      const pageNumber = parseInt(page)
-      const pageSize = parseInt(perPage)
+      const { page, resultPerPage, keyword, location } = req.query;
 
-      if (isNaN(pageNumber) || pageNumber < 1) {
-        res.status(400).json({message: "Numero de pagina invalido"})
-      }
-      if (isNaN(pageSize) || pageSize  < 1) {
-        res.status(400).json({message: "Cantidad de vacantes por pagina invalida"})
-      }
-      const allJobs: Job[] = await this.service.getJobs();
-      const totalJobs = allJobs.length
-      const totalPages = Math.ceil(totalJobs / pageSize)
+      const { paginated, total, totalPages } = await this.service.getJobs({
+        keyword,
+        location,
+        pageNumber,
+        resultPerPage,
+      });
 
-      const startIndex = (pageNumber - 1) * pageSize
-      const endIndex = pageNumber * pageSize
-      const paginatedJobs = allJobs.slice(startIndex, endIndex)
-      res.json({ results: paginatedJobs, totalJobs, totalPages, currentPage: pageNumber, perPage: pageSize });
+      res.json({
+        results: paginated,
+        totalJobs: total,
+        totalPages,
+        currentPage: page,
+        resultPerPage,
+      });
     } catch (e) {
-      console.error("Error en getJobs:", e);
+      this.logger.writeError(`Error en getJobs: ${e}`);
       res.status(500).json({ message: "Error interno del servidor" });
     }
   };
