46af89b54ca76e1abcfda4a64acf6910cba47204
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-03 18:53:03 -0600

    feat: se uso el hook useIntersectionObserver para observer cuando el panel de informacion de la vacante llegaba a cierta altura y poder posicionarlo de forma fija

--- frontend/src/layouts/JobsLayout.tsx
+++ frontend/src/layouts/JobsLayout.tsx
@@ -1,34 +1,58 @@
-import { JobList } from '@/features/jobs/components/JobList/JobList';
-import { Outlet, useLocation } from 'react-router';
-import data from "@/resources/json/mockApiData.json";
-import { Job } from "@/types";
+import { JobList } from "@/features/jobs/components/JobList/JobList";
+import { Outlet, useOutletContext } from "react-router";
+import { useResponsive } from "@/hooks/useResponsive";
+import { useAppSelector } from "@/app/hooks";
+import { selectAllJobs } from "@/features/jobs/jobs.selectors";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import notFoundImage from "@/resources/search-no-result-found.png";
 
-const jobs: Job[] = data;
-const myJobs: Job[] = [];
+type OutletContext = {
+  separatorRef: React.RefObject<HTMLDivElement>;
+};
 
 export function JobsLayout() {
-  const location = useLocation()
-  const basePath = location.pathname.includes("vacantes") ? "vacantes" : "recomendaciones"
+  const {separatorRef} = useOutletContext<OutletContext>();
+  const {isOutOfView} = useIntersectionObserver({ref: separatorRef, threshold: 1, rootMargin: "-64px 0px 0px 0px"})
+  const jobs = useAppSelector(selectAllJobs);
+  const { breakpoint } = useResponsive();
 
-  const toggleSaveJob = (id: string) => () => {};
-
-  const isJobSaved = (id: string) => myJobs.some((job) => job.id === id);
+  if (jobs.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <picture>
+          <img
+            src={notFoundImage}
+            className="w-sm object-contain aspect-[1/1]"
+          />
+        </picture>
+        <div className="max-w-2xl">
+          <h2 className="text-gray-400 text-xl font-bold tracking-wide">No hay empleos que coincidan con tu búsqueda "aqui va la busqueda"</h2>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className="grid grid-cols-2 gap-x-4 min-h-screen">
-      {/* Lista fija a la izquierda */}
-        <JobList
-          items={jobs}
-          toggleSaveJob={toggleSaveJob}
-          isJobSaved={isJobSaved}
-          path={basePath}
-        />
-      {/* Detalle dinámico a la derecha */}
-      <div className="bg-neutral-900">
-        <h2>detalles</h2>
-        <Outlet />
-      </div>
+    <div
+      className={`grid gap-x-5 w-full ${
+        breakpoint === "mobile" || breakpoint === "mobileLarge"
+          ? "grid-cols-1"
+          : "grid-cols-2"
+      }`}
+    >
+      <JobList items={jobs} />
+      {breakpoint.includes("mobile") ? (
+        <div>{/* Panel de la descripcion de la vacante */}</div>
+      ) : (
+        <div
+
+          className={`transition-all duration-300 overflow-auto ${
+            isOutOfView ? "sticky top-20 bg-neutral-900 h-[calc(100vh-80px)] rounded-xl z-10" : null
+          } p-6 border-[1px] border-neutral-800 relative rounded-md`}
+        >
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 }
-
