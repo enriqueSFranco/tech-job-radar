a5a03d41bc4a5934cfe11ed18b66e02547111fe6
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-04 16:37:42 -0600

    feat:(rsbuild) add react-scan script tag to HTML output

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -5,10 +5,21 @@ export default defineConfig({
   plugins: [pluginReact()],
   source: {
     define: {
-      'import.meta.env.API_URL': JSON.stringify(process.env.API_URL),
+      'import.meta.env.BASE_API_URL': JSON.stringify(process.env.BASE_API_URL),
       'import.meta.env.API_JOBS': JSON.stringify(process.env.API_JOBS),
       'import.meta.env.API_RECOMMENDATIONS': JSON.stringify(process.env.API_RECOMMENDATIONS),
       'import.meta.env.PORT': JSON.stringify(process.env.PORT),
     },
   },
+  html: {
+    tags: [
+      {
+        tag: "script",
+        attrs: {
+          src: "https://cdn.jsdelivr.net/npm/react-scan/dist/auto.global.js",
+        },
+        append: false,
+      }
+    ]
+  }
 });
