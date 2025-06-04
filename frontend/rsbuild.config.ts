764d04fd042b1985eb7f6545dc542e36b9c67e68
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-03 18:58:31 -0600

    feat: se agrego configuracion para poder usar variables de entorno

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -3,4 +3,12 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    define: {
+      'import.meta.env.API_URL': JSON.stringify(process.env.API_URL),
+      'import.meta.env.API_JOBS': JSON.stringify(process.env.API_JOBS),
+      'import.meta.env.API_RECOMMENDATIONS': JSON.stringify(process.env.API_RECOMMENDATIONS),
+      'import.meta.env.PORT': JSON.stringify(process.env.PORT),
+    },
+  },
 });
