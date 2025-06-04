b5c3a309bf7745f6321719299b7da4f26e522b6a
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-03 18:54:11 -0600

    refactor: se refactorizo la forma en la que renderizaba la informacion

--- frontend/src/features/jobs/components/JobList/JobList.tsx
+++ frontend/src/features/jobs/components/JobList/JobList.tsx
@@ -1,30 +1,33 @@
+import { useState } from "react";
 import { Job } from "@/types";
 import { Card } from "@/ui/organisms/Card/Card";
 
 interface Props {
   items: Job[];
-  path: string
-  toggleSaveJob(id: string): () => void;
-  isJobSaved(id: string): boolean;
 }
 
-export function JobList({ items, path, toggleSaveJob, isJobSaved }: Props) {
+export function JobList({ items }: Props) {
+  const [selectedCardJob, setSelectedCardJob] = useState<Pick<
+    Job,
+    "id"
+  > | null>(items.length > 0 ? { id: items[0].id } : null);
+
   return (
     <div data-testid="job-list-container" className="w-full">
       <ul
         aria-label="Lista de empleos disponibles"
         role="list"
         data-testid="job-list"
-        className="w-full space-y-6 grid grid-flow-row md:auto-cols-min divide-white/20 lg:flex lg:flex-wrap lg:justify-center lg:items-center"
+        className="w-full space-y-3 grid"
       >
         {items.map((item) => (
-          <li key={`jobId-${item.id}`} data-testid={`job-item-${item.id}`} className="w-full">
-            <Card
-              item={item}
-              onSaveJob={() => toggleSaveJob(item.id)}
-              isSave={isJobSaved(item.id)}
-              path={path}
-            />
+          <li
+            key={`jobId-${item.id}`}
+            data-testid={`job-item-${item.id}`}
+            className="w-full"
+            onClick={() => setSelectedCardJob({ id: item.id })}
+          >
+            <Card item={item} isSelected={selectedCardJob?.id === item.id} />
           </li>
         ))}
       </ul>
