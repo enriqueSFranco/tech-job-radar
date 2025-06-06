9c6729785090ef7ccf0ae08e0bc2d388067d1481
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-06 11:29:10 -0600

    feat: se agregaron nuevos estados de recuperacion de las vacantes

--- frontend/src/features/jobs/components/JobList/JobList.tsx
+++ frontend/src/features/jobs/components/JobList/JobList.tsx
@@ -1,33 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Job } from "@/types";
 import { Card } from "@/ui/organisms/Card/Card";
+import { JobsLoader } from "../JobsLoader";
+import { NoJobsFound } from "@/views/jobs/not-found";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { selectJobsStatus } from "../../jobs.selectors";
+import { ErrorMessage } from "@/ui/atoms/ErrorMessage";
+import { selectFormSearch } from "@/features/search/search.selectors";
+import { fetchJobs } from "../../jobs.thunks";
+import { useLocation, useNavigate } from "react-router";
 
 interface Props {
-  items: Job[];
+  jobs: Job[];
 }
 
-export function JobList({ items }: Props) {
+export function JobList({ jobs }: Props) {
   const [selectedCardJob, setSelectedCardJob] = useState<Pick<
     Job,
     "id"
-  > | null>(items.length > 0 ? { id: items[0].id } : null);
+  > | null>(jobs.length > 0 ? { id: jobs[0].id } : null);
+  const { keyword, location } = useAppSelector(selectFormSearch);
+  const navigate = useNavigate();
+  const locationPath = useLocation().pathname;
+  const dispatch = useAppDispatch();
+  const jobsStatus = useAppSelector(selectJobsStatus);
+
+  useEffect(() => {
+    if (jobsStatus === "idle")
+      dispatch(
+        fetchJobs({
+          keyword: "frontend developer",
+          location: "ciudad de mexico",
+        })
+      );
+  }, [jobsStatus, dispatch]);
+
+  useEffect(() => {
+    if (
+      jobsStatus === "succeeded" &&
+      jobs.length > 0 &&
+      (locationPath === "/empleos" || locationPath === "/recomendaciones")
+    ) {
+      navigate(`${locationPath}/${jobs[0].id}`, { replace: true });
+    }
+  }, [jobsStatus, jobs, locationPath, navigate]);
+
+  if (jobsStatus === "pending") return <JobsLoader />;
+
+  if (jobsStatus === "failed")
+    return <ErrorMessage message="Error loading jobs." />;
+
+  if (jobs.length === 0 && jobsStatus === "succeeded")
+    return <NoJobsFound keyword={keyword} location={location} />;
 
   return (
-    <div data-testid="job-list-container" className="w-full">
+    <div data-testid="job-list-container" className="col-span-5">
       <ul
         aria-label="Lista de empleos disponibles"
         role="list"
         data-testid="job-list"
-        className="w-full space-y-3 grid"
+        className="grid columns-lg gap-y-5"
       >
-        {items.map((item) => (
+        {jobs.map((job) => (
           <li
-            key={`jobId-${item.id}`}
-            data-testid={`job-item-${item.id}`}
-            className="w-full"
-            onClick={() => setSelectedCardJob({ id: item.id })}
+            key={`jobId-${job.id}`}
+            data-testid={`job-job-${job.id}`}
+            onClick={() => setSelectedCardJob({ id: job.id })}
           >
-            <Card item={item} isSelected={selectedCardJob?.id === item.id} />
+            <Card item={job} isSelected={selectedCardJob?.id === job.id} />
           </li>
         ))}
       </ul>
