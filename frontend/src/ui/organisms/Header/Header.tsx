09e76dacd131e290def3930a469e02354951b35c
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-06 11:22:07 -0600

    styles: se cambio el posicionamiento de fixed a sticky

--- frontend/src/ui/organisms/Header/Header.tsx
+++ frontend/src/ui/organisms/Header/Header.tsx
@@ -3,23 +3,19 @@ import { useScrollTrigger } from "@/ui/organisms/Header/useScrollTrigger";
 interface HeaderProps {
   leftSlot?: React.ReactNode;
   rightSlot?: React.ReactNode;
+  isPanelOpen?: boolean;
 }
 
-export function Header({ leftSlot, rightSlot }: HeaderProps) {
+export function Header({ leftSlot, rightSlot, isPanelOpen }: HeaderProps) {
   const isScrolled = useScrollTrigger();
 
   return (
     <header
       aria-label="main header"
-      className={`px-4 flex items-center justify-between gap-2 w-full h-16 fixed top-0 z-50
-    bg-black/40 backdrop-blur-md shadow-md border-b border-black/20
-    transition-all duration-300
-    ${isScrolled ? "bg-black/40 backdrop-blur-md" : "bg-black"}
-    text-white
-  `}
+      className={`px-4 flex items-center justify-between w-full h-16 sticky top-0 z-50 shadow-sm border-b border-b-neutral-800 transition-all duration-300 text-white ${isPanelOpen ? "bg-blue-700" : null} ${isScrolled ? "bg-black/40 backdrop-blur-md" : "bg-black"}`}
     >
-      <div>{leftSlot}</div>
-      <div>{rightSlot}</div>
+      {leftSlot}
+      <div className="flex items-center justify-center">{rightSlot}</div>
     </header>
   );
 }
