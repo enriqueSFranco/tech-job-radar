ed1d297830cfbb05489703e44b4f237407a33749
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-06 11:21:47 -0600

    feat: se cambio el nombre de la ruta vacantes a empleos

--- frontend/src/navigation/App.tsx
+++ frontend/src/navigation/App.tsx
@@ -1,28 +1,15 @@
-import { Navigate, Route, Routes } from "react-router";
-import { JobsLayout } from "@/layouts/JobsLayout";
+import { Route, Routes } from "react-router";
 import { Home } from "@/views/app/Home";
-// import { AllJobs } from "@/views/jobs/all-jobs";
-// import { JobsForYou } from "@/views/jobs/jobs-for-you";
 import { Login } from "@/views/auth/Login";
 import { Register } from "@/views/auth/Register";
-import { JobDetails } from "@/views/jobs/job-details";
+import { JobExplorer } from "@/views/app/JobExplorer";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />}>
-        <Route index element={<Navigate to="/vacantes" />} />
-
-        <Route path="vacantes" element={<JobsLayout />}>
-          {/* <Route index element={<AllJobs />} /> */}
-          <Route path=":jobId" element={<JobDetails />} />
-        </Route>
-        <Route path="recomendaciones" element={<JobsLayout />}>
-          {/* <Route index element={<JobsForYou />} /> */}
-          <Route path=":jobId" element={<JobDetails />} />
-        </Route>
+        <Route path="empleos/:slug" element={<JobExplorer />} />
       </Route>
-
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
