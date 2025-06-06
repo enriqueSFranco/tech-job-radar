cc609d364fba0323acf19c9804f55def3188b11e
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-06 11:27:45 -0600

    feat: configuracion de rsbuild para poder acceder a las variables de entorno

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="@rsbuild/core/types" />
+
+interface ImportMetaEnv {
+  readonly API_URL: string;
+  readonly API_JOBS: string;
+  readonly API_RECOMMENDATIONS: string;
+  readonly PORT: string;
+  // agrega aquí otras variables que uses
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
