8de05ba3fa262aeb11bfc0da37c0ea816bcb7d63
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-06-05 19:10:23 -0600

    style(card): cambia de color el background  cuando una card se selecciona

--- frontend/src/ui/organisms/Card/Card.tsx
+++ frontend/src/ui/organisms/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import type { Job } from "@/types";
 import { Chip } from "@/ui/atoms/Chip";
@@ -11,9 +11,10 @@ interface Props {
 }
 
 export function Card({ item, isSelected }: Props) {
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const savedJobs = useAppSelector((state) => state.savedJobs.savedJobs);
-
   const isSaved = savedJobs.some((j) => j.id === item.id);
 
   function handleSaveToggle(e: React.MouseEvent) {
@@ -26,84 +27,81 @@ export function Card({ item, isSelected }: Props) {
     }
   }
 
+  function handleClick() {
+    const currentPathname = location.pathname;
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("jobId", item.id);
+    navigate(`${currentPathname}?${searchParams.toString()}`);
+  }
+
   return (
-    <div className="relative flex flex-row-reverse">
-      <button
-        onClick={handleSaveToggle}
-        data-testid={`save-button-${item.id}`}
-        className="cursor-pointer self-start absolute right-2 top-2"
-        aria-label={
-          isSaved ? "Quitar este emplo de guardados" : "Guardar estem empleo"
-        }
-        title={isSaved ? "Quitar de guardados" : "Guardar"}
-        aria-pressed={isSaved}
-      >
-        <IcBookMark
-          className={
-            isSaved ? "fill-white" : "fill-transparent stroke-gray-500"
-          }
-        />
-      </button>
-      <Link
-        to={`/vacantes/${item.id}`}
-        className={`${
-          isSelected ? "border-blue-600" : "border-neutral-800"
-        } p-2 flex flex-col justify-bwetween gap-2 bg-white/5 xl:outline xl:outline-neutral-900 overflow-hidden w-full h-full rounded-xl border-[1px]`}
-        aria-labelledby={`job-title-${item.id}`}
-        role="region"
-        data-testid={`job-card-${item.id}`}
-      >
-        <header className="flex items-start justify-between text-sm">
-          <div className="flex flex-col items-start justify-start gap-2">
-            <h2
-              id={`job-title-${item.id}`}
-              data-testid={`job-title-${item.id}`}
-              className="font-bold text-lg tracking-wide"
-            >
-              {item.jobTitle}
-            </h2>
-            <div className="flex items-center justify-start gap-1">
-              <div className="flex items-center gap-1.5">
-                <h3 className="text-sm text-gray-400 self-end">
-                  {item.companyName}
-                </h3>
-              </div>
-              <span className="text-gray-500">&#9679;</span>
-              <h3 className="text-sm text-gray-400 self-end">
-                {item.jobLocation}
-              </h3>
-            </div>
-          </div>
-        </header>
-        <div role="group" aria-label="Etiquetas del empleo" className="mt-2">
-          <ul
-            className="flex items-center flex-wrap gap-2"
-            data-testid={`job-tags-${item.id}`}
+    <article
+      onClick={handleClick}
+      className={`relative cursor-pointer transform transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:-translate-y-2 flex flex-col gap-2 rounded-xl w-full ${isSelected ? "bg-blue-700/15 border-[1px]" : "border-neutral-800"} xl:outline xl:outline-neutral-900 p-4`}
+      aria-labelledby={`job-title-${item.id}`}
+      role="region"
+      data-testid={`job-card-${item.id}`}
+      tabIndex={0}
+    >
+      <header className="flex items-start justify-between text-sm">
+        <div className="flex flex-col items-start gap-2">
+          <h2
+            id={`job-title-${item.id}`}
+            data-testid={`job-title-${item.id}`}
+            className="font-bold text-lg tracking-wide"
           >
-            <li>
-              <Chip label={item.isRemote ? "Desde casa" : "En oficina"} />
-            </li>
-            <li>
-              <Chip label={item.employmentType} />
-            </li>
-          </ul>
-        </div>
-        {item.jobDescription && (
-          <div className="grow">
-            <p
-              className="text-balance text-sm text-left text-gray-300 tracking-wide"
-              data-testid={`job-description-${item.id}`}
-            >
-              {item.jobDescription}
-            </p>
+            {item.jobTitle}
+          </h2>
+          <div className="flex items-center gap-1">
+            <h3 className="text-sm text-gray-400">{item.companyName}</h3>
+            <span className="text-gray-500">&#9679;</span>
+            <h3 className="text-sm text-gray-400">{item.jobLocation}</h3>
           </div>
-        )}
-        <footer className="flex items-start justify-between">
-          <label className="text-gray-400 text-sm font-light">
-            {item.datePosted}
-          </label>
-        </footer>
-      </Link>
-    </div>
+        </div>
+        <button
+          onClick={handleSaveToggle}
+          data-testid={`save-button-${item.id}`}
+          className="cursor-pointer"
+          aria-label={
+            isSaved ? "Quitar este empleo de guardados" : "Guardar este empleo"
+          }
+          title={isSaved ? "Quitar de guardados" : "Guardar"}
+          aria-pressed={isSaved}
+        >
+          <IcBookMark
+            className={
+              isSaved ? "fill-white" : "fill-transparent stroke-gray-500"
+            }
+          />
+        </button>
+      </header>
+
+      <div role="group" aria-label="Etiquetas del empleo" className="mt-2">
+        <ul
+          className="flex flex-wrap gap-2"
+          data-testid={`job-tags-${item.id}`}
+        >
+          <li>
+            <Chip label={item.isRemote ? "Desde casa" : "En oficina"} />
+          </li>
+          <li>
+            <Chip label={item.employmentType} />
+          </li>
+        </ul>
+      </div>
+
+      {item.jobDescription && (
+        <p
+          className="text-sm text-gray-300 tracking-wide"
+          data-testid={`job-description-${item.id}`}
+        >
+          {item.jobDescription}
+        </p>
+      )}
+
+      <footer className="flex justify-between text-sm text-gray-400 font-light">
+        <label>{item.datePosted}</label>
+      </footer>
+    </article>
   );
 }
